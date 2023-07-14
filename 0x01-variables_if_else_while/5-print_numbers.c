89ed18cb052b082c96d57f829a8aeef5924481d0
YKOLAZ247 <Olayinkaazeez247@gmail.com>
2023-07-14 10:07:37 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - The Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int g;
+
+	for (g = 0; g < 10; g++)
+		printf("%d", g);
+	printf("\n");
+	return (0);
+}
