69e411764a1ed4034d54176bf246ab7404075f6a
YKOLAZ247 <Olayinkaazeez247@gmail.com>
2023-07-22 19:42:43 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,14 @@
 
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
+	char i, ch;
 
-	for (i = 0 ; i < 10; i++)
+	for (i = 0 ; i <= 9; i++)
 	{
-		for (ch = 'a' ; ch <= 'z' ; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
 			_putchar(ch);
-	}
+		}
 	_putchar('\n');
+	}
 }
